4623168b328c3d65b56955e47845b93191da0c62
Carlos Villavicencio <carlos.po5i@gmail.com>
2017-03-08 15:29:47 -0600

    Exportar resultados en json

--- jrv/views.py
+++ jrv/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect
 from .models import *
 from .forms import *
 from django.core.files.storage import FileSystemStorage
+from django.core import serializers
 
 def index(request):
     context = {}
@@ -14,55 +15,16 @@ def resultados(request):
     return render(request, 'jrv/resultados.html', context)
 
 def opendata(request):
-    data = {
-        'some_var_1': 'foo',
-        'some_var_2': 'bar',
-    }
-    return JsonResponse(data)
+    data = serializers.serialize("json", Acta.objects.all())
+    return HttpResponse(data, content_type='application/json')
 
 
 @csrf_protect
 def register(request):
-    """
-    provincia = request.POST.get("provincia")
-    parroquia = request.POST.get("parroquia")
-    circunscripcion = request.POST.get("circunscripcion")
-    zona = request.POST.get("zona")
-    canton = request.POST.get("canton")
-    junta = request.POST.get("junta")
-    lasso = request.POST.get("lasso")
-    moreno = request.POST.get("moreno")
-    blancos = request.POST.get("blancos")
-    nulos = request.POST.get("nulos")
-    autor = request.POST.get("autor")
-    useragent = request.POST.get("useragent")
-    latitude = request.POST.get("latitude")
-    longitude = request.POST.get("longitude")
-    foto = request.FILES.get('file')
-    """
     
     # TODO: backend validation
     
     try:
-        """
-        acta = Acta()
-        acta.provincia = provincia
-        acta.parroquia = parroquia
-        acta.circunscripcion = circunscripcion
-        acta.zona = zona
-        acta.canton = canton
-        acta.junta = junta
-        acta.lasso = lasso
-        acta.moreno = moreno
-        acta.blancos = blancos
-        acta.nulos = nulos
-        acta.autor = autor
-        acta.useragent = useragent
-        acta.latitude = latitude
-        acta.longitude = longitude
-        #acta.foto = foto
-        acta.save()
-        """
         form = ActaForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
