615f0d8d2c5793248dea184dc5a5522021692bc8
Rafael Rivadeneira <rafariva@gmail.com>
2017-03-10 15:22:34 -0500

    cambio nombre pagina voluntario

--- jrv/urls.py
+++ jrv/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-	url(r'^contactenos/$', views.contactenos, name='contactenos'),
+    url(r'^voluntario/$', views.voluntario, name='voluntario'),
     url(r'^resultados/$', views.resultados, name='resultados'),
     url(r'^opendata/$', views.opendata, name='opendata'),
     url(r'^register/$', views.register, name='register'),
